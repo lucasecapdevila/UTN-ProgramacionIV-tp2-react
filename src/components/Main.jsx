b60d441a7c1dda47f1d1f0ad3ae4619b04b52f2c
Lucas Capdevila <lcapdevila60@gmail.com>
2025-09-18 16:07:10 -0300

    Datos agregados a card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import { jugadoresArgentina } from '../utils/jugadores'
-import { Button, Card } from 'react-bootstrap'
+import React from "react";
+import { jugadoresArgentina } from "../utils/jugadores";
+import { Card, Col, Container, ListGroup, Row } from "react-bootstrap";
 
 const Main = () => {
   return (
-    <main>
-      {jugadoresArgentina.map(jugador => (
-        <Card key={jugador.numero}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      ))}
+    <main className="py-5 px-2 px-md-4 px-lg-5 mainPage">
+      <h1 className="text-center mb-4">Jugadores de la selección de Argentina - Qatar 2022</h1>
+      <Container fluid>
+        <Row>
+          {jugadoresArgentina.map((jugador) => (
+            <Col xs={12} sm={6} md={4} lg={3} key={jugador.numero} className="">
+              <Card className="mb-4">
+                <Card.Img
+                  className="img-fluid border-bottom"
+                  variant="top"
+                  src={jugador.img}
+                />
+                <Card.Body>
+                  <Card.Title className="text-center">
+                    {jugador.nombre}
+                  </Card.Title>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item><span className="fw-bold">Posición:</span> {jugador.posicion.charAt(0).toUpperCase() + jugador.posicion.slice(1)}</ListGroup.Item>
+                    <ListGroup.Item><span className="fw-bold">Edad:</span> {jugador.edad} años</ListGroup.Item>
+                    <ListGroup.Item><span className="fw-bold">Estatura:</span> {jugador.estatura}</ListGroup.Item>
+                    <ListGroup.Item><span className="fw-bold">Peso:</span> {jugador.peso}</ListGroup.Item>
+                    <ListGroup.Item><span className="fw-bold">Club:</span> {jugador.club}</ListGroup.Item>
+                    <ListGroup.Item><span className="fw-bold">Goles:</span> {jugador.goles}</ListGroup.Item>
+                    <ListGroup.Item><span className="fw-bold">Asistencias:</span> {jugador.asistencias}</ListGroup.Item>
+                  </ListGroup>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </main>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
