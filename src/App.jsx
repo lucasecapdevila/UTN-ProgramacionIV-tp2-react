913210114fca21656ae2a2e53d64287303e47675
Brun0 <brunosigh91@gmail.com>
2025-09-18 21:05:01 -0300

    adición del Header+nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer"
 import Main from "./components/Main"
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <>
+      <Header />
       <Main />
       <Footer />
     </>
